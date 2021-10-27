e58930473b1ab69c0c801f13896bca2341318a88
ha1999 <leha220699@gmail.com>
2021-10-27 20:35:36 +0700

    Initial API with FastApi

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return {
+        "message": "Hello World"
+    }
+
+
+@app.get('/items/{item_id}')
+async def getItem(item_id: int):
+    return {
+        "item_id": item_id
+    }
